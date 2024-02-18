297b893c113af2873017860724cf0d6407782f9b
Shamiivan <shamiivan@gmail.com>
2024-02-18 03:46:36 -0500

    added predection script

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import cv2
-import torch
 from ultralytics import YOLO
 
-# import ultralytics.yolo.v8.detect.predict
-
 # Load the pretrained weights
-weights_path = "./runs/detect/train/weights/last.pt"  # Replace with the path to your YOLOv8 weights file
+weights_path = "./runs/detect/train/weights/best.pt"  # Replace with your weights path
 model = YOLO(weights_path)
 
-model.predict(source="0", show=True, conf=0.5)
+# Perform inference and store the results in a variable
+results = model("./img.jpg")  # List of Results objects
+
+# print(results)
