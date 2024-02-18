873dda5833cf4ceea20dfb10abc08c7ec6a0412f
KhaledMat <khaledmatloub2001@gmail.com>
2024-02-18 07:59:55 -0500

    locking mechanism

--- motorSetup.py
+++ motorSetup.py
@@ -14,6 +14,8 @@ INPUT2_PIN = 19
 INPUT3_PIN = 13
 INPUT4_PIN = 6
 
+LOCK_PIN = 5
+
 IR_PIN = 23
 
 PWM_FREQUENCY = 1000  # 1 kH
@@ -25,6 +27,7 @@ GPIO.setup(INPUT1_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(INPUT2_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(INPUT3_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(INPUT4_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(LOCK_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Set GPIO pins as output
 GPIO.setup(IN1, GPIO.OUT)
@@ -40,72 +43,98 @@ pwm_in2 = GPIO.PWM(IN2, PWM_FREQUENCY)
 pwm_in3 = GPIO.PWM(IN3, PWM_FREQUENCY)
 pwm_in4 = GPIO.PWM(IN4, PWM_FREQUENCY)
 
-GPIO.add_event_detect(INPUT1_PIN, GPIO.FALLING)
-GPIO.add_event_detect(INPUT2_PIN, GPIO.FALLING)
-GPIO.add_event_detect(INPUT3_PIN, GPIO.FALLING)
-GPIO.add_event_detect(INPUT4_PIN, GPIO.FALLING)
+
 
 # Function to turn the motor clockwise
-def clockwise():
-    pwm_in1.start(70)
-    pwm_in2.start(0)
-    pwm_in3.start(70)
-    pwm_in4.start(0)
+def clockwise(num):
+    if num == 0:
+        pwm_in1.start(70)
+        pwm_in2.start(0)
+    elif num == 1:
+        pwm_in3.start(70)
+        pwm_in4.start(0)
 
 # Function to turn the motor counter-clockwise
-def counterclockwise():
-    pwm_in1.start(0)
-    pwm_in2.start(70)
-    pwm_in3.start(0)
-    pwm_in4.start(70)
+def counterclockwise(num):
+    if num == 0:
+        pwm_in1.start(0)
+        pwm_in2.start(70)
+    elif num == 1:
+        pwm_in3.start(0)
+        pwm_in4.start(70)
 
 # Stop the motor
-def stop():
-    pwm_in1.start(0)
-    pwm_in2.start(0)
-    pwm_in3.start(0)
-    pwm_in4.start(0)
+def stop(num):
+    if num == 0:
+        pwm_in1.start(0)
+        pwm_in2.start(0)
+    elif num == 1:
+        pwm_in3.start(0)
+        pwm_in4.start(0)
 
 
-def brake():
-    pwm_in1.start(100)
-    pwm_in2.start(100)
-    pwm_in3.start(100)
-    pwm_in4.start(100)
+def brake(num):
+    if num == 0:
+        pwm_in1.start(100)
+        pwm_in2.start(100)
+    elif num == 1:
+        pwm_in3.start(100)
+        pwm_in4.start(100)
 
 def moveToContainer(container):
+
     if container == 0:
+        GPIO.add_event_detect(INPUT1_PIN, GPIO.FALLING)
         while True:
-            counterclockwise()
+            counterclockwise(0)
             if GPIO.event_detected(INPUT1_PIN):
-                brake()
+                brake(0)
                 break
     elif container == 1:
+        GPIO.add_event_detect(INPUT2_PIN, GPIO.FALLING)
         while True:
-            counterclockwise()
+            counterclockwise(0)
             if GPIO.event_detected(INPUT2_PIN):
-                brake()
+                brake(0)
                 break
     elif container == 2:
+        GPIO.add_event_detect(INPUT3_PIN, GPIO.FALLING)
         while True:
-            counterclockwise()
+            counterclockwise(0)
             if GPIO.event_detected(INPUT3_PIN):
-                brake()
+                brake(0)
                 break
     elif container == 3:
+        GPIO.add_event_detect(INPUT4_PIN, GPIO.FALLING)
         while True:
-            counterclockwise()
+            counterclockwise(0)
             if GPIO.event_detected(INPUT4_PIN):
-                brake()
+                brake(0)
                 break
-            
+
+def unlock():
+    clockwise(1)
+    time.sleep(5)
+    stop(1)
+
+def lock():
+    GPIO.add_event_detect(LOCK_PIN, GPIO.RISING)
+    while True:
+        counterclockwise(1)
+        if GPIO.event_detected(LOCK_PIN):
+                brake(1)
+                break
 
 def detectTrash():
+
     # find out the type of garbage
     trashType = 1
     moveToContainer(trashType)
+    unlock()
+    time.sleep(10)
+    lock()
 
-# Test the motor
+
 try:
     while True:
         sensor_state = GPIO.input(IR_PIN)
@@ -117,30 +146,7 @@ try:
         input2_state = GPIO.input(INPUT2_PIN)
         input3_state = GPIO.input(INPUT3_PIN)
         input4_state = GPIO.input(INPUT4_PIN)
-
-
-        # counterclockwise()
-        # # If input is high, call clockwise() function
-
-        # if GPIO.event_detected(INPUT1_PIN):
-        #     print("4")
-        #     brake()
-            # break
-        # if GPIO.event_detected(INPUT2_PIN):
-        #     print("2")
-        #     brake()
-        #     time.sleep(2)
-        # if GPIO.event_detected(INPUT3_PIN):
-        #     print("3")
-        #     brake()
-        #     time.sleep(2)
-        # if GPIO.event_detected(INPUT4_PIN):
-        #     print("4")
-        #     brake()
-        #     time.sleep(2)
-        # If input is low, stop the motor and break the loop
     
-
 
 except KeyboardInterrupt:
     print("Interrupted by user")
--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 import cv2
+import RPi.GPIO as GPIO
+
+IR_PIN = 23
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(IR_PIN, GPIO.IN)
 
 # Create VideoCapture object
 cap = cv2.VideoCapture(0)  # 0 represents the first USB camera, 1 for the second, and so on
@@ -7,16 +13,21 @@ cap = cv2.VideoCapture(0)  # 0 represents the first USB camera, 1 for the second
 if not cap.isOpened():
     print("Error: Could not open camera.")
 else:
+    while True:
+        sensor_state = GPIO.input(IR_PIN)
+        if sensor_state == GPIO.HIGH:
+
     # Capture a single frame
-    ret, frame = cap.read()
+            ret, frame = cap.read()
 
     # Check if frame captured successfully
-    if ret:
+            if ret:
         # Save the captured frame as an image file
-        cv2.imwrite('captured_image.jpg', frame)
-        print("Image captured successfully.")
-    else:
-        print("Error: Failed to capture frame.")
+                cv2.imwrite('captured_image.jpg', frame)
+                print("Image captured successfully.")
+            else:
+                print("Error: Failed to capture frame.")
 
-    # Release the VideoCapture object
-    cap.release()
+            # Release the VideoCapture object
+            cap.release()
+            break
